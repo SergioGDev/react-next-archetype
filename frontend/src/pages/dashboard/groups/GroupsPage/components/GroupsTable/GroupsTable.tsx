e7b9acc76ce25e9ce91a81889f08b32509ef47ae
SergioGDev <admin@sergiogdev.com>
2024-03-14 23:27:16 +0100

    feat: groups table request

--- frontend/src/pages/dashboard/groups/GroupsPage/components/GroupsTable/GroupsTable.tsx
+++ frontend/src/pages/dashboard/groups/GroupsPage/components/GroupsTable/GroupsTable.tsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import GeneralTable from "@/components/common/GeneralTable/GeneralTable";
 import Spinner from "@/components/common/Spinner";
 import { useGroupsTable } from "./hooks/useGroupsTable";
 import { getRowsGroupsData } from "./groupsTable.helper";
 import { headersGroupDataTable } from "./groupsTable.consts";
-import { useGetData } from "@/hooks/useGetData";
-import { GroupListPageApiRespData } from "@/types/group.types";
+import { GroupData, GroupListPageApiRespData } from "@/types/group.types";
 
 import { useRouter } from "next/navigation";
 import { Box, Button, Typography } from "@mui/material";
 import { Add } from "@mui/icons-material";
+import { getRequest } from "@/helpers/request/getRequest";
 
 const GroupsTable = () => {
   const { userData, actionsGroupTable } = useGroupsTable();
+  const [isLoading, setisLoading] = useState(true);
+  const [data, setData] = useState<GroupData[]>();
   const router = useRouter();
+
+  useEffect(() => {
+    if (userData) {
+      setisLoading(true);
+      getRequest<GroupListPageApiRespData>(
+        "/api/group/groups",
+        getQueryParams()
+      ).then((resp) => {
+        const { groups } = resp.data;
+        setData(groups);
+        setisLoading(false);
+      });
+    }
+  }, [userData]);
 
   const getQueryParams = (): { [key: string]: string } => {
     const queryParams: { [key: string]: string } = {};
@@ -24,17 +40,12 @@ const GroupsTable = () => {
     return queryParams;
   };
 
-  const { data, isLoading } = useGetData<GroupListPageApiRespData>(
-    "/api/group/groups",
-    getQueryParams()
-  );
-
   if (isLoading) return <Spinner />;
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
       <Typography variant="h4">Groups</Typography>
-      
+
       {userData?.role !== "USER_ROLE" && (
         <Box
           sx={{
@@ -56,7 +67,7 @@ const GroupsTable = () => {
       )}
 
       <GeneralTable
-        tableRows={getRowsGroupsData(data?.groups)}
+        tableRows={getRowsGroupsData(data)}
         tableHeaders={headersGroupDataTable}
         tableActions={actionsGroupTable}
       />
